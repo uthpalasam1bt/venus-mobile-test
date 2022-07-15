221baff64a837e8955a910fb8fb253e6a5ee3634
Uthpala Samarakoon <uthpala.samarakoon@1billiontech.com>
2022-07-15 16:50:06 +0530

    steps ui fix

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,15 +1,26 @@
-import { NavigationContainer } from "@react-navigation/native";
-import {WhiteSpace,Steps, WingBlank } from '@ant-design/react-native'
-import React, { useRef, useState } from "react";
-import { Button, DrawerLayoutAndroid, Text, StyleSheet, View, ScrollView } from "react-native";
-import { createDrawerNavigator ,  DrawerContentScrollView } from '@react-navigation/drawer';
-import { Collapse, CollapseBody, CollapseHeader } from "../collapseMenu";
+import {NavigationContainer} from '@react-navigation/native';
+import {WhiteSpace, Steps, WingBlank} from '@ant-design/react-native';
+import React, {useRef, useState} from 'react';
+import Icon from 'react-native-vector-icons/dist/AntDesign';
+import {
+  Button,
+  DrawerLayoutAndroid,
+  Text,
+  StyleSheet,
+  View,
+  ScrollView,
+} from 'react-native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+} from '@react-navigation/drawer';
+import {Collapse, CollapseBody, CollapseHeader} from '../collapseMenu';
 
-const Step=Steps.Step
-const Drawer = createDrawerNavigator()
+const Step = Steps.Step;
+const Drawer = createDrawerNavigator();
 function Feed() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Feed Screen</Text>
     </View>
   );
@@ -17,94 +28,167 @@ function Feed() {
 
 function Article() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Article Screen</Text>
     </View>
   );
 }
 
 function CustomDrawerContent(props) {
-  const _renderView=()=>{
+  const _renderView = () => {
     return (
       <View>
         <Text>wwew</Text>
       </View>
-    )
-  }
-  const _renderCollapseView=()=>{
+    );
+  };
+  const _renderCollapseView = () => {
     return (
       <View>
         <Steps>
-          <Step title="sd"/>
+          <Step title="sd" />
         </Steps>
       </View>
-    )
-  }
+    );
+  };
   return (
     <DrawerContentScrollView {...props}>
-      <View>
-      <Collapse>
-      <CollapseHeader>
-        <WingBlank bordered>
-          <Text>Workflow</Text>
-        </WingBlank>
-      </CollapseHeader>
-      <CollapseBody>
-        <Steps >
-          <Step title="ssd"></Step>
-        </Steps>
-       
-      </CollapseBody>
-    </Collapse>
+      <View style={{margin: 20}}>
+        <Collapse>
+          <CollapseHeader>
+            <Text style={{color: 'black', fontSize: 18}}>Master Workflow</Text>
+          </CollapseHeader>
+
+          <CollapseBody>
+            <View>
+              <Steps>
+                <Step
+                  renderIcon={({starting, waiting, error}) => (
+                    <View style={styles.icon}>
+                      <Icon name="check" color="white" />
+                    </View>
+                  )}
+                  title={
+                    <View>
+                      <Text style={styles.steptext}>
+                        Activate Classification
+                      </Text>
+                    </View>
+                  }
+                  status="finish"></Step>
+
+                <Step
+                  renderIcon={({starting, waiting, error}) => (
+                    <View style={styles.icon}>
+                      <Icon name="check" color="white" />
+                    </View>
+                  )}
+                  title={
+                    <View>
+                      <Text style={styles.steptext}>
+                        Activate Classification
+                      </Text>
+                    </View>
+                  }
+                  status="finish"></Step>
+                <Step
+                  renderIcon={({starting, waiting, error}) => (
+                    <View style={styles.icon}>
+                      <Icon name="close" color="white" />
+                    </View>
+                  )}
+                  title={
+                    <View>
+                      <Text style={styles.steptext}>
+                        Activate Classification
+                      </Text>
+                    </View>
+                  }
+                  status="error"></Step>
+                <Step
+                  renderIcon={({starting, waiting, error}) => (
+                    <View style={styles.icon}>
+                      <Icon name="check" color="white" />
+                    </View>
+                  )}
+                  title={
+                    <View>
+                      <Text style={styles.steptext}>
+                        Activate Classification
+                      </Text>
+                    </View>
+                  }
+                  status="progress"></Step>
+                <Step
+                  renderIcon={({starting, waiting, error}) => (
+                    <View style={styles.icon}>
+                      <Icon name="check" color="white" />
+                    </View>
+                  )}
+                  title={
+                    <View>
+                      <Text style={styles.steptext}>
+                        Activate Classification
+                      </Text>
+                    </View>
+                  }
+                  status="wait"></Step>
+              </Steps>
+            </View>
+          </CollapseBody>
+        </Collapse>
       </View>
-      
-    
     </DrawerContentScrollView>
   );
 }
 const Sidebar = () => {
-let drawer
+  let drawer;
 
-  const onOpenChange = (isOpen) => {
+  const onOpenChange = isOpen => {
     /* tslint:disable: no-console */
-    console.log('是否打开了 Drawer', isOpen.toString())
-  }
-
-  
-
+    console.log('是否打开了 Drawer', isOpen.toString());
+  };
 
   return (
     <>
-    <Drawer.Navigator useLegacyImplementation
-
-    drawerContent={(props) => <CustomDrawerContent {...props} />}
-    >
-      <Drawer.Screen  name="Feed" component={Feed} />
-      <Drawer.Screen name="Article" component={Article} />
-    </Drawer.Navigator>
+      <Drawer.Navigator
+        useLegacyImplementation
+        drawerContent={props => <CustomDrawerContent {...props} />}>
+        <Drawer.Screen name="Feed" component={Feed} />
+        <Drawer.Screen name="Article" component={Article} />
+      </Drawer.Navigator>
     </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex:1
+    flex: 1,
   },
   navigationContainer: {
-    backgroundColor: "#ecf0f1"
+    backgroundColor: '#ecf0f1',
   },
   paragraph: {
     padding: 16,
     fontSize: 15,
-    textAlign: "center"
+    textAlign: 'center',
   },
-  drawer_button:{
-    position:'absolute',
-    width:30,
+  drawer_button: {
+    position: 'absolute',
+    width: 30,
     top: 0,
     left: 0,
-    paddingTop:60,
-  }
+    paddingTop: 60,
+  },
+  steptext: {
+    fontSize: 15,
+    color: 'black',
+  },
+  icon: {
+    justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+  },
 });
 
 export default Sidebar;
--- components/collapseMenu/CollapseHeader.js
+++ components/collapseMenu/CollapseHeader.js
@@ -1,50 +1,60 @@
 import React from 'react';
-import {View,
-    StyleSheet,} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 Feather.loadFont();
-const CollapseHeader = React.forwardRef(({children,showArrow,show,
-    arrowSize,arrowColor, arrowStyle,customArrowUp,customArrowDown,...restProps}, ref) => {
-      
-        const opacity = show? 0.5 : 1;
-        return (
-    <View ref={ref} {...restProps}>
-         
-                            <View style={[styles.arrow]}>
-                            {children}
-                            <View style={[arrowStyle, { opacity }]}>
-                                {
-                                    show ? (
-                                       customArrowDown(arrowSize, arrowColor)
-                                    ) : (
-                                            customArrowUp(arrowSize, arrowColor)
-                                        )
-                                }
-                            </View>
-                        </View>
- 
- 
-    </View>
-  );
-});
+const CollapseHeader = React.forwardRef(
+  (
+    {
+      children,
+      showArrow,
+      show,
+      arrowSize,
+      arrowColor,
+      arrowStyle,
+      customArrowUp,
+      customArrowDown,
+      ...restProps
+    },
+    ref,
+  ) => {
+    const opacity = show ? 0.5 : 1;
+    return (
+      <View ref={ref} {...restProps} style={{paddingBottom: 20}}>
+        <View style={[styles.arrow]}>
+          {children}
+          <View style={[arrowStyle, {opacity}]}>
+            {show
+              ? customArrowUp(arrowSize, arrowColor)
+              : customArrowDown(arrowSize, arrowColor)}
+          </View>
+        </View>
+      </View>
+    );
+  },
+);
 
-CollapseHeader.defaultProps={
-    arrowStyle: {},
-    arrowColor: '#000',
-    showArrow: true,
-    arrowSize: 15,
-    customArrowUp: (size, color) => <Feather name="chevron-up" size={size} color={color} />,
-    customArrowDown: (size, color) => <Feather name="chevron-down" size={size} color={color} />,
-}
+CollapseHeader.defaultProps = {
+  arrowStyle: {},
+  arrowColor: '#000',
+  showArrow: true,
+  arrowSize: 15,
+  customArrowUp: (size, color) => (
+    <Feather name="chevron-up" size={size} color={color} />
+  ),
+  customArrowDown: (size, color) => (
+    <Feather name="chevron-down" size={size} color={color} />
+  ),
+};
 
 const styles = StyleSheet.create({
-    arrow: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingVertical: 8,
-        borderTopLeftRadius: 0,
-        borderBottomLeftRadius: 0,
-    },})
+  arrow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+  },
+});
 
 export default CollapseHeader;
